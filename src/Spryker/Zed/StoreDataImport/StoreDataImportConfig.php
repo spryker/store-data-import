6f9d750491d9c62be39852da1fc0bb35f75aa08d
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Zed/StoreDataImport/StoreDataImportConfig.php
+++ src/Spryker/Zed/StoreDataImport/StoreDataImportConfig.php
@@ -13,6 +13,8 @@ use Spryker\Zed\DataImport\DataImportConfig;
 class StoreDataImportConfig extends DataImportConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const IMPORT_TYPE_STORE = 'store';
